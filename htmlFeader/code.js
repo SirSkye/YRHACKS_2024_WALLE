0202e6567975335b10f733977a0f4774a87f9651
EEEE EEEE <urmom.com>
2024-04-18 14:21:20 -0400

    base html in place (it doesnt work)

---
+++ htmlFeader/code.js
@@ -0,0 +1,7 @@
+function changePage(){
+    // similar behavior as an HTTP redirect
+    window.location.replace("test.html");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "test.html";
+ }; 
---
+++ htmlFeader/htmlFeader.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Request, Depends, HTTPException, status, APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2AuthorizationCodeBearer, OAuth2PasswordRequestForm, OAuth2PasswordBearer
+# from pydantic_models import *
+# from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from typing import Annotated
+
+
+client = FastAPI()
+router=APIRouter()
+# client.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+client.include_router(router)
+
+SECRET_KEY = "aba1f4e7fac9ac0b03d6d5b49d0ad592dd09abddd7f2330d4905f65f6a823092"
+ALGORITHM = "HS256" #Hashing
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+client.run(degbug=True)
+
+@router.get("/login",tags=["login"])
+
+async def login(request:Request):
+    context={"request":request}
+    return templates.testLogin(request=context,name="testLogin.html")
+
+@client.get("/", response_class=HTMLResponse)
+async def root():
+    return ""
+    #return """<h3 onclick="myFunction(this, 'red')">Click me to change my color.</h3>
+
+
+@client.get("/home/", response_class=HTMLResponse)
+@client.get("/", response_class=HTMLResponse)
+async def give_home_html(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse(request=context, name="home.html")
+
+@client.get("/test/", response_class=HTMLResponse)
+async def give_test_htmp(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse(request=context, name="test.html")
